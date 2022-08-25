f24d8778403b09330353eaff8d475dce1caebefa
Parzival8971 <esh8971@gmail.com>
2022-08-26 02:15:30 +0900

    Vue.js 라이플사이클 훅을 파악한다

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,32 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>{{ count }}</h1>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      count : 2
+    }
+  },
+  beforeCreate() {
+    console.log('Before Create!')
+  },
+  created() {
+    console.log('Created!', this.count)
+    console.log(document.querySelector('h1'))
+  },
+  beforeMount() {
+    console.log('Before Mount!')
+    console.log(document.querySelector('h1'))
+  },
+  mounted() {
+    console.log('Mounted!')
+    console.log(document.querySelector('h1'))
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
